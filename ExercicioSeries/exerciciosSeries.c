27d3da70f7720632ca9d71c99e2c30dac6ce6e46
André Vinícius Trentini <87705035+whoisdede@users.noreply.github.com>
2023-08-22 15:25:11 -0300

    Add files via upload

---
+++ ExercicioSeries/exerciciosSeries.c
@@ -0,0 +1,219 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+//declaração de funções
+void imprimeMenu();             
+int fat(int n);                 
+void harmonica();               
+void somatorioLog();       
+void calculaPi();
+void calculaE();
+void serieTaylor();
+void senoDeX();
+void cosDeX();
+
+//função main
+int main(){
+    int select =-1;
+    while (select !=0){
+        imprimeMenu();
+        printf("\nSelecione um caso para prosseguir: ");
+        scanf(" %d", &select);
+        switch (select)
+        {
+        case 0:
+            break;
+        case 1:
+            harmonica();
+            break;
+        case 2:
+            somatorioLog(); 
+            break;
+        case 3:
+            calculaPi();
+            break;
+        case 4:
+            calculaE();
+            break;
+        case 5:
+            serieTaylor();
+            break;
+        case 6:
+            senoDeX();
+            break;
+        case 7:
+            cosDeX();
+            break;
+        default:
+            printf("\n valor inválido");
+            break;
+        }
+    }
+}
+
+//funções:
+
+int fat(int n){
+    double fat;
+    if(n == 0 || n == 1){
+        fat = 1;
+    }   
+    else{
+        for (fat=1; n>1; n--){
+        fat *= n;
+        }   
+    }
+    return fat;  
+}
+
+void harmonica(){
+    int k=0;
+    float result=0;
+    printf("digite um valor para k: ");
+    scanf(" %d", &k);
+    for(int i=1; i<=k; i++){
+        float a =i;
+        result += 1/a;
+    }
+    printf("\n Aqui está o resultado: %f", result);
+}
+
+void somatorioLog(){
+    int k=0;
+    float result=0;
+    double pot=-1;
+    printf("digite um valor para k: ");
+    scanf(" %d", &k);
+    for(int i=1; i<=k; i++){
+        float a = i;
+        pot *= -1;
+        result += pot/a ;   
+    }
+    printf("\n Aqui está o resultado: %f", result);
+}
+
+void calculaPi(){
+    int select =-1;
+    while (select !=0){
+        int k=0;
+        float result = 0;
+        printf("\nSelecione uma das formas de calcular: \n1 para Leibniz\n2 para Nilakantha\nOu 0 para sair\n");
+        scanf(" %d", &select);
+        switch(select)
+        {
+            case 0:
+                break;
+            case 1:
+                printf("digite um valor para k: ");
+                scanf(" %d", &k);
+                int inverte = -1;
+                for(int i=1; i<=k; i++){
+                    float a=i;
+                    inverte *= -1;
+                    result += (4*inverte)/(2*a-1);
+                }
+                printf("\n Aqui está o resultado: %f", result);
+                break;  
+            case 2:
+                printf("digite um valor para k: ");
+                scanf(" %d", &k);
+                int inverto = -1;
+                float n = 2;
+                for(int i=1; i<=k; i++){        
+                    float a=i;
+                    inverto *= -1;
+                    result += ((4*inverto)/(n*(n+1)*(n+2)));
+                    n +=2;
+                }
+                result += 3;
+                printf("\n Aqui está o resultado: %f", result);
+                break;
+            default:
+                printf("valor invalido,");
+                break;
+        }
+    }
+}
+
+void calculaE(){
+    int k=0; 
+    float result=0;
+    printf("digite um valor para k: ");
+    scanf(" %d", &k);
+    for(int i=0; i<=k; i++){
+        double fato = fat(i);
+        result += 1/fato;  
+    }
+    printf("\n Aqui está o resultado: %f", result);
+}
+
+void serieTaylor(){
+    float x=0;
+    int n=0,i,j;
+    printf("Digite x e a quantidade de termos n: ");
+    scanf(" %f %d", &x, &n);
+    float e_x =0;
+    for(i=0;i<=n;i++){
+        double termo = 1;
+        for(j=1;j<=i;j++){
+            termo *= x/j;
+        }
+        e_x += termo;
+    }
+    printf("e elevado a %.3f = %.8f\n", x, e_x);
+}
+
+void senoDeX(){
+    float result=0, x=0;
+    int k=0;
+    int inverto = 1;
+    printf("Digite a quantidade de termos k: ");
+    scanf(" %d", &k);
+    printf("\nPois bem, agora digite um numero para x: ");
+    scanf(" %f", &x);
+    for(int i=1; i<=k; i++){
+        inverto *= -1;
+        float a=0;
+        int n =(2*i)+1;
+        for( int j=1; j<n;j++){
+            a += x*x;
+        }
+        result += (inverto*(a))/(fat(n));
+    }
+    result += x;
+    printf("\n Aqui está o resultado: %f", result);
+}
+
+void cosDeX(){
+    float result=0, x=0;
+    int k=0;
+    int inverto = 1;
+    printf("Digite a quantidade de termos k: ");
+    scanf(" %d", &k);
+    printf("\nPois bem, agora digite um numero para x: ");
+    scanf(" %f", &x);
+    for(int i=1; i<=k; i++){
+        inverto *= -1;
+        float a=0;
+        int n =(2*i);
+        for( int j=1; j<n;j++){
+            a += x*x;
+        }
+        result += (inverto*(a))/(fat(n));
+    }
+    result += 1;
+    printf("\n Aqui está o resultado: %f", result);
+}
+
+void imprimeMenu(){
+    printf("\nEscolha uma opcao:");
+    printf("\n0 - Sair");
+    printf("\n1 - Série Harmônica");
+    printf("\n2 - Somatorio de Log");
+    printf("\n3 - Calcular Pi");
+    printf("\n4 - calculaE");
+    printf("\n5 - Serie de Taylor");
+    printf("\n6 - Seno de X");
+    printf("\n7 - Cosseno de X");
+}
---
+++ SelecaoRepeticao/selecao.c
@@ -0,0 +1,257 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void imprimeMenu();
+void coord();
+void tipoTriangulo();
+void horario();
+void pares();
+void medias();
+void somaImpares();
+void somaImpares2();
+
+int main(){
+    int select =-1;
+    while (select !=0){
+        imprimeMenu();
+        printf("\nSelecione um caso para prosseguir: ");
+        scanf(" %d", &select);
+        switch (select)
+        {
+        case 0:
+            break;
+        case 1:
+            coord();
+            break;
+        case 2:
+            tipoTriangulo();
+            break;
+        case 3:
+            horario();
+            break;
+        case 4:
+            pares();
+            break;
+        case 5:
+            medias();
+            break;
+        case 6:
+            somaImpares();
+            break;
+        case 7:
+            somaImpares2();
+            break;
+        default:
+            printf("\n valor inválido");
+            break;
+        }
+    }
+}
+void imprimeMenu(){
+    printf("\nEscolha uma opcao:");
+    printf("\n0 - Sair");
+    printf("\n1 - Calcula Quadrante");
+    printf("\n2 - Tipo do Triângulo");
+    printf("\n3 - Calcula Horario Jogo");
+    printf("\n4 - Valores Pares/Impares/Negativos/Positivos");
+    printf("\n5 - Média Ponderada");
+    printf("\n6 - Soma de ímpares consecutivos");
+    printf("\n7 - Soma de ímpares consecutivos 2");
+}
+void coord(){
+    double x=0,y=0;
+    printf("\ndigite o valor para x: ");
+    scanf(" %lf", &x);
+    printf("\ndigite o valor para y: ");
+    scanf(" %lf", &y);
+    if(x ==0 && y ==0){
+        printf
+        ("\nOrigem!");
+    }
+    else if(y==0){
+        printf("\nEixo X");
+    }
+    else if(x==0){
+        printf("\nEixo Y");
+    }
+    else if (x>0 && y>0){
+        printf("\nQ1");
+    }
+    else if (x<0 && y>0){
+        printf("\nQ2");
+    }
+    else if (x<0 && y<0){
+        printf("\nQ3");
+    }
+    else if(x>0 && y<0){
+        printf("\nQ4");
+    }
+    printf("\n");
+}
+
+void tipoTriangulo(){
+    float A=0,B=0, C=0;
+    printf("\ndigite o valor para A: ");
+    scanf(" %f", &A);
+    printf("\ndigite o valor para B: ");
+    scanf(" %f", &B);
+    printf("\ndigite o valor para C: ");
+    scanf(" %f", &C);
+    if(A>=B+C){
+        printf("\nNão forma triângulo!");
+    }
+    if(A*A == B*B + C*C){
+        printf("\nTriângulo retângulo");
+    }
+    if(A*A > B*B + C*C){
+        printf("\nTriângulo obtusângulo");
+    }
+    if(A*A < B*B + C*C){
+        printf("\nTriângulo acutângulo");
+    }
+    if(A == B && A == C && B==C){
+        printf("\nTriângulo equilátero");
+    }
+    if((A == B && A != C && B!=C) || (A==C && B!=C && A !=B) || (B==C && A!=C && A!=B)){
+        printf("\nTriângulo isósceles");
+    }
+    printf("\n");
+}
+
+void horario(){
+    int x=0,y=0;
+    printf("\nDigite um valor para x: ");
+    scanf(" %d", &x);
+    printf("\nDigite um valor para y: ");
+    scanf(" %d", &y);
+    if(x==y){
+        printf("\nO jogo durou 24 horas");
+    }
+    else if(x<y){
+        printf ("\nO jogo durou %d horas", y-x);
+    }
+    else if(x>y){
+        printf("\nO jogo durou %d horas", 24-x+y);
+    }
+    printf("\n");
+}
+
+void pares(){
+    int n=0, pares=0, impares=0, positivos=0, negativos=0;
+    printf("\nDigite quantos valores quer colocar e em seguida os valores: ");
+    scanf(" %d", &n);
+    int loop = n;
+    for(int i=0; i<loop; i++){
+        if(n%2 == 0){
+            pares++;
+        }
+        else{
+            impares++;
+        }
+        if(n==0){
+            pares++;
+            impares--;
+        }
+        else if (n>0){
+            positivos++;
+        }
+        else if (n<0){
+            negativos++;
+        }
+        printf("-> ");
+        scanf(" %d", &n);
+    }
+    printf("\n%d valores pares", pares);
+    printf("\n%d valores impares", impares);
+    printf("\n%d valores positivos", positivos);
+    printf("\n%d valores negativos", negativos);
+    printf("\n");
+}
+
+void medias(){
+    int n=0;
+    double x=0,y=0,z=0;
+    double medias[100];
+    printf("\nDigite quantos valores quer colocar: ");
+    scanf(" %d", &n);
+    for(int i=0; i<n; i++){
+        printf("\ndigite os 3 valores: ");
+        scanf(" %lf", &x);
+        scanf(" %lf", &y);
+        scanf(" %lf", &z);
+        medias[i] = ((x*2)+(y*3)+(z*5))/10;
+    }
+    for(int i=0; i<n; i++){
+        printf("\nmedia %d: %.1lf", i+1, medias[i]);
+    }
+    printf("\n");
+}
+
+void somaImpares(){
+    int x=0, y=0;
+    int soma=0;
+    printf("\nDigite um valor para x: ");
+    scanf(" %d", &x);
+    printf("\nDigite um valor para y: ");
+    scanf(" %d", &y);
+    if(x>y){
+        while(y<x){
+            y++;
+            x--;
+            if(y%2 != 0){
+                soma+=y;
+            }
+        }
+    }
+    else if(y>x){
+        while(x<y){
+            x++;
+            y--;
+            if(x%2 != 0){
+                soma+=x;
+            }
+        }
+    }
+    else{
+        printf("\nsão iguais");
+    }
+    printf("\n%d\n", soma);
+}
+void somaImpares2(){
+    int n=0;
+    int soma[100];
+    printf("\nDigite um um valor para n:");
+    scanf(" %d", &n);
+    for(int i=0; i<n; i++){
+        int x=0, y=0;
+        printf("\nDigite um valor para x: ");
+        scanf(" %d", &x);
+        printf("\nDigite um valor para y: ");
+        scanf(" %d", &y);
+        if(x-y == 1 || y-x == 1){
+            soma[i] += 0; 
+        }
+        else{
+            if(x>y){
+                while(x>y){
+                    y++;
+                    x--;
+                    if(y%2 != 0){
+                        soma[i]+=y;
+                    }
+                }
+            }
+            else if(y>x){
+                while(y>x){
+                    x++;
+                    if(x%2 != 0){
+                        soma[i]+=x;
+                    }
+                }
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        printf("\n%d\n", soma[i]);
+    }
+}
